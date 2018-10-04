e069fa59970a21fd32c17a9da55d0fe154e70175
snesin <snesin@gmail.com>
2018-10-04 09:09:32 -0500

    First pass

---
+++ Complex/{$FILE}/{$FILE+Component}.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { {$NAME}Component } from './{$FILE+Component}';
+
+describe('{$NAME}Component', () => {
+  let component: {$NAME}Component;
+  let fixture: ComponentFixture<{$NAME}Component>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ {$NAME}Component ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent({$NAME}Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Simple/{#FILE+Dialog}.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Observable } from 'rxjs';
+
+import { DialogService } from '../DialogService';
+
+import { {#NAME}DialogComponent, I{#NAME}DialogData, I{#NAME}DialogResponse } from './{#FILE+DialogComponent}.ts';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class {#NAME}Dialog {
+  constructor(private dialogService: DialogService, private dialog: MatDialog) {
+  }
+  public open(data:I{#NAME}DialogData): Observable<I{#NAME}DialogResponse | undefined> {
+    return this.dialog.open({#NAME}DialogComponent, this.dialogService.getDialogConfig({#WIDTH}, data)).afterClosed();
+  }
+}
